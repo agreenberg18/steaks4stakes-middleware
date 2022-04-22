0d9f26e57f86427c208c6fde881dd72e51e11890
agreenberg18 <agreenberg18@users.noreply.github.com>
2022-04-21 23:26:43 -0400

    Mistakes were made - real root commit

--- stakesapp.js
+++ stakesapp.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config();
 
-const GoogleMapsKey = process.env.Google_Places_Key
+const MONGOPASS = process.env.MONGO_PW
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://greenberga:${MONGOPASS}@steaks4stakes.vgfrc.mongodb.net/steaks4stakes?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 app.get('/', function (req, res) {
-  res.send(GoogleMapsKey)
+  res.send('hello world')
 })
 
 app.listen(3000)
